6b68b3a7338b69746588066ee504281172195d4f
MiranDoDo <dreadforum@atomicmail.io>
2026-04-08 04:19:59 -0400

    Changes:
    - Fix error when you don't select file and click play
    Exception in Tkinter callback
    Traceback (most recent call last):
      File "/usr/lib/python3.13/tkinter/__init__.py", line 2071, in __call__
        return self.func(*args)
               ~~~~~~~~~^^^^^^^
      File "/home/x7/Documents/chuvak_player/media.py", line 45, in play_music
        pygame.mixer.music.load(music_path)
        ~~~~~~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^
    pygame.error: not a file object
    
    - Change buttons font & add buttons color
    
    - Add music loop when the music ends
    
    - Upgrade code structure

--- media.py
+++ media.py
@@ -1,50 +1,65 @@
 # Импорты
+
 from tkinter import *
-from tkinter import filedialog
-from tkinter import messagebox
-from tkinter import ttk
+from tkinter import filedialog, messagebox, ttk
 import tkinter as tk
 import pygame
+import time
 
 # Обязательная инциализация
+
 pygame.mixer.pre_init(44100, -16, 2, 2048)
 pygame.mixer.init()
 
-
 # Окно
+
 window = Tk()
 window.title("Chuvak Player")
 window.geometry("570x600")
 window.maxsize(570, 600)
 window.minsize(570, 600)
+window.style = ttk.Style(window)
+window.style.configure('TButton', font=('Helvetica', 11), background='#d1ea8f')
+
 
 # Переменные
-chuvak = PhotoImage(file = "./assets/backgrounds/chuvak.png")
-music = ""
+
+background_chuvak = PhotoImage(file = "./assets/backgrounds/chuvak.png")
+music_path = ""
 
 
 class MediaPlay:
 
-#"""Документация к классу MediaPlay
-# Используется для выбора, воспроизведения, остановки и паузы музыки"""
+#               Документация к классу MediaPlay
+# Используется для выбора, воспроизведения, остановки и паузы музыки
 
-    def __init__(self, media):
-        self.media = media
+    def __init__(self):
+        pass
+
 
     def play_music(self):
-        global music
-        if music != "":
-         pygame.mixer.music.load(music)
-         pygame.mixer.music.play()
-         print(f"Play {music}")
-        elif music == "":
+        global music_path
+        if music_path == "":
             messagebox.showerror("Error", "Error! You need to select file fisrt.")
+        elif music_path != "":
+         pygame.mixer.music.load(music_path)
+         time.sleep(0.1) # Маленькая задержка
+         pygame.mixer.music.play(-1)
+         print(f"Play {music_path}")
 
-    def get_music(self):
-        global music
-        media = filedialog.askopenfilename(filetypes=[("MP3 Files",('*.mp3')), ("FLAC Files",('*.flac')), ("WAV Files",('*.wav'))])
-        music = media
-        print(music)
+    def select_music(self):
+        global music_path
+        music = filedialog.askopenfilename(filetypes=[
+            ("MP3 Files",('*.mp3')),
+            ("FLAC Files",('*.flac')),
+            ("WAV Files",('*.wav'))]
+        )
+        # Проверка типа и пути файла
+        if type(music) == tuple:
+            pass
+        elif music != "":
+            music_path = music
+            print(music_path)
 
     def stop_music(self):
         pygame.mixer.music.stop()
@@ -55,25 +70,59 @@ class MediaPlay:
     def unpause_music(self):
         pygame.mixer.music.unpause()
 
+musicplay = MediaPlay()
 
-musicplay = MediaPlay(music)
+
+## UI
 
 # Задний фон
-labelchuvak = Label(window, image = chuvak)
-labelchuvak.place(x = 0, y = 0)
+label_background = Label(window, image = background_chuvak)
+label_background.place(x = 0, y = -45)
 
+# Label для кнопок
+
+label_buttons = Label(window,
+bg='#0b9b92',
+width=150,
+height=3
+)
+label_buttons.place(x=0, y=553)
 
 # Кнопки
 
-open_button = ttk.Button(window, text="Open file", command=musicplay.get_music)
-open_button.place(x=232, y=564)
-play_button = ttk.Button(window, text="Start music", command=musicplay.play_music)
+play_button = ttk.Button(
+window,
+text="Start music",
+command=musicplay.play_music
+)
 play_button.place(x=5, y=564)
-stop_button = ttk.Button(window, text="Stop music", command=musicplay.stop_music)
+
+open_button = ttk.Button(
+window,
+text="Open file",
+command=musicplay.select_music
+)
+open_button.place(x=232, y=564)
+
+stop_button = ttk.Button(
+window,
+text="Stop music",
+command=musicplay.stop_music
+)
 stop_button.place(x=88, y=564)
-pause_button = ttk.Button(window, text="Pause music", command=musicplay.pause_music)
-pause_button.place(x=367, y=564)
-unpause_button = ttk.Button(window, text="Unpause music", command=musicplay.unpause_music)
+
+pause_button = ttk.Button(
+window,
+text="Pause music",
+command=musicplay.pause_music
+)
+pause_button.place(x=364, y=564)
+
+unpause_button = ttk.Button(
+window,
+text="Unpause music",
+command=musicplay.unpause_music
+)
 unpause_button.place(x=457, y=564)
 
 window.mainloop()
