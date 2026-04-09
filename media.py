7439e4293138c86c568ee1ce79c2b35d761055eb
MiranDoDo <dreadforum@atomicmail.io>
2026-04-09 13:14:32 -0400

    [C# Update
    What’s new:
    
    - Add menu
    
    - 2 function(pause & unpause) now in 1 button
    
    - Update for music name label
    
    - Change button’s geometry

--- media.py
+++ media.py
@@ -1,5 +1,4 @@
 # Импорты
-
 from tkinter import *
 from tkinter import filedialog, messagebox, ttk
 import tkinter as tk
@@ -21,11 +20,11 @@ window.minsize(570, 600)
 window.style = ttk.Style(window)
 window.style.configure('TButton', font=('Helvetica', 11), background='#d1ea8f')
 
-
 # Переменные
 
 background_chuvak = PhotoImage(file = "./assets/backgrounds/chuvak.png")
 music_path = ""
+IS_PLAY = False
 
 
 class MediaPlay:
@@ -39,10 +38,12 @@ class MediaPlay:
 
     def play_music(self):
         global music_path
+        global IS_PLAY
         if music_path == "":
             messagebox.showerror("Error", "Error! You need to select file fisrt.")
         elif music_path != "":
          pygame.mixer.music.load(music_path)
+         IS_PLAY = True
          time.sleep(0.1) # Маленькая задержка
          pygame.mixer.music.play(-1)
          print(f"Play {music_path}")
@@ -61,25 +62,61 @@ class MediaPlay:
              music_path = music
              print(music_path)
              # Нахождение имени музыки
-             music_name1 = music_path.rfind("/")
-             music_name2 = music_path[music_name1+1:len(music_path)]
-             label_name.config(text = music_name2)
-             
+             music_name1 = music_path.rfind("/") # Удаляет путь к файлу, оставляя только его название
+             music_name2 = music_path.rfind(".") # Удаляет расширение файла
+             music_name3 = music_path[music_name1+1:music_name2]
+             label_name.config(font=('Helvetica', 11), text = music_name3)
+
     def stop_music(self):           
         pygame.mixer.music.stop()
 
     def pause_music(self):
-        pygame.mixer.music.pause()
-
-    def unpause_music(self):
-        pygame.mixer.music.unpause()
+        global IS_PLAY
+        if IS_PLAY == True: # Проверка: Играет ли музыка
+            pygame.mixer.music.pause() # Если нет, то ставит на паузу
+            IS_PLAY = False
+        elif IS_PLAY == False: # Иначе:
+            pygame.mixer.music.unpause() # Убирает с паузы
+            IS_PLAY = True
 
 musicplay = MediaPlay()
 
 
 ## UI
 
+# Меню
+
+menubar = Menu(window,
+bg='#0b9b92',
+borderwidth=3.2)
+file = Menu(menubar, tearoff = 0)
+
+menubar.add_cascade(label ='File ',
+menu = file,
+background='#d1ea8f',
+activebackground='#d1ea8f',
+font=('Helvetica', 11)
+)
+
+file.add_command(label ='Open Audio',
+command = musicplay.select_music,
+background='#d1ea8f',
+activebackground='#d1ea8f',
+font=('Helvetica', 11)
+)
+
+file.add_separator(background='#d1ea8f')
+
+file.add_command(label ='Exit',
+command = window.destroy,
+background='#d1ea8f',
+activebackground='#d1ea8f',
+font=('Helvetica', 11)
+)
+
+
 # Задний фон
+
 label_background = Label(window, image = background_chuvak)
 label_background.place(x = 0, y = -45)
 
@@ -91,54 +128,48 @@ width=150,
 height=3,
 text=""
 )
-label_buttons.place(x=0, y=553)
+label_buttons.place(x=0, y=510)
 
 # Label для названия музыки
 
 label_name = Label(window,
-bg='#0b9b92',
+bg="#107c75",
 fg='#ffffff',
 width=40,
 height=3,
-font=('Helvetica', 11)
+font=('Helvetica', 11),
+relief="raised",
+text="In the Silence.."
 )
-label_name.place(x=120, y=510)
+label_name.pack(side=BOTTOM)
 
 # Кнопки
 
 play_button = ttk.Button(
 window,
-text="Start music",
+text="Play",
 command=musicplay.play_music
 )
-play_button.place(x=5, y=564)
-
-open_button = ttk.Button(
-window,
-text="Open file",
-command=musicplay.select_music
-)
-open_button.place(x=232, y=564)
+play_button.place(x=5, y=513)
 
 stop_button = ttk.Button(
 window,
-text="Stop music",
+text="Stop",
 command=musicplay.stop_music
 )
-stop_button.place(x=88, y=564)
+stop_button.place(x=5, y=539)
+
+BPstyle = ttk.Style()
+BPstyle.configure('Large.TButton', padding=9)
 
 pause_button = ttk.Button(
 window,
-text="Pause music",
-command=musicplay.pause_music
+text="Pause",
+command=musicplay.pause_music,
+style="Large.TButton"
 )
-pause_button.place(x=364, y=564)
+pause_button.place(x=470, y=520)
 
-unpause_button = ttk.Button(
-window,
-text="Unpause music",
-command=musicplay.unpause_music
-)
-unpause_button.place(x=457, y=564)
 
+window.config(menu = menubar)
 window.mainloop()
